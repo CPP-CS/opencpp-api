f9b698e6bd9f7dccd163973d4c65ccb216e12450
Luciano Lim <chunyumikey@gmail.com>
2021-10-09 17:03:30 -0700

    remove debug

--- classesAPI.py
+++ classesAPI.py
@@ -5,7 +5,7 @@ from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 cors = CORS(app)
-app.config["DEBUG"] = True
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 DATABASE = 'Courses.db'
