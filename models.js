4875b7f49999a83221104e1aae7dc1481419a70d
Luciano Lim <lmlim@cpp.edu>
2022-02-24 11:22:34 -0800

    fixes database name

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("database", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize("db", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   maxConcurrentQueries: 100,
   dialect: "mysql",
