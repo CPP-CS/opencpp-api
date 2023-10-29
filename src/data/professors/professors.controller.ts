b0a7ecb8ad6b689fe709212d64c37d331d0c805e
Luciano Lim <lmlim@cpp.edu>
2023-10-29 15:01:57 -0400

    adds broncodirectme endpoints

--- src/data/professors/professors.controller.ts
+++ src/data/professors/professors.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Professor } from 'src/db/db';
 
 @Controller()
 export class ProfessorsController {
   constructor() {}
+
+  @Post('find')
+  async find(
+    @Body() body: { InstructorFirst: string; InstructorLast: string },
+  ) {
+    const professors = await Professor.findAll({
+      where: {
+        LastName: body.InstructorLast,
+        FirstName: body.InstructorFirst,
+      },
+    });
+    return professors.map((professor) => {
+      return {
+        id: professor.id.valueOf(),
+        InstructorFirst: professor.FirstName,
+        InstructorLast: professor.LastName,
+        TotalEnrollment: professor.GradePoints,
+        AvgGPA: professor.AvgGPA,
+        Label: professor.FirstName + ' ' + professor.LastName,
+      };
+    });
+  }
 
   @Post('findAll')
   async findAll() {
